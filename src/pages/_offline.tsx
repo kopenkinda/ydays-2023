8df047792d939ead528135953d3294e2ece8e9ae
Dmitrii <kopenkin.da@gmail.com>
2022-11-16 17:12:43 +0100

    pwa added basic support

---
+++ src/pages/_offline.tsx
@@ -0,0 +1,9 @@
+import { Box, Text } from '@chakra-ui/react';
+
+export default function OfflinePage() {
+  return (
+    <Box className='grid place-items-center'>
+      <Text size='xl'>You&apos;re offline</Text>
+    </Box>
+  );
+}
