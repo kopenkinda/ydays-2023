2102359193af760524d46f0099551b3c72f0b7ee
Gabriel Cancel <gabrielcancel66@gmail.com>
2023-02-08 16:36:09 +0100

    commit

--- src/components/article/article.tsx
+++ src/components/article/article.tsx
@@ -1,33 +1,34 @@
 import { IconClockHour2 } from '@tabler/icons-react';
 import readingTime from 'reading-time';
-import type { Post } from '@prisma/client';
 
-export default function Article({ article }: { article: Post }) {
-  const { content, isFree, published, title, id } = article;
-  const stats = readingTime(content);
-  if (stats.minutes < 1) {
-    stats.minutes = 1;
-  }
+import type { Article as TArticle } from '../../pages/articles';
+
+export default function Article({ article }: { article: TArticle }) {
+  const { title, slug, mainImage } = article;
+  // const stats = readingTime(content);
+  // if (stats.minutes < 1) {
+  //   stats.minutes = 1;
+  // }
   return (
     <div className='mt-6'>
-      <a href={`/posts/${id}`}>
+      <a href={`/posts/${slug}`}>
         <div
           className='relative h-32 rounded-md  border-2 border-white'
           style={{
-            backgroundImage: `url(${''})`,
+            backgroundImage: `url(${mainImage})`,
             backgroundSize: 'cover',
             backgroundPosition: 'center',
           }}
         >
           <div className='absolute bottom-0 right-4 flex h-8 flex-row'>
             <IconClockHour2 className='mr-2' />
-            <p>{stats.minutes} min</p>
+            {/* <p>{stats.minutes} min</p> */}
           </div>
         </div>
       </a>
 
       <h2 className='text-lg font-semibold'>{title}</h2>
-      <p>{content.length > 50 ? content.slice(0, 50) + ' ...' : content}</p>
+      {/* <p>{content.length > 50 ? content.slice(0, 50) + ' ...' : content}</p> */}
     </div>
   );
 }
